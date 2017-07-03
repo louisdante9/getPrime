d456c0386a75ac266a8027c1b4b7f6f2c0121613
louisdante <mase2kool@gmail.com.au>
2017-07-03 17:27:37 +0100

    pushing getPrime function code to git

---
+++ README.md
@@ -0,0 +1,2 @@
+# getPrime
+this is a javascript function that takes an integer value **n** as argument and generates prime numbers from 0 to n. The generated prime numbers would be **in an array**
---
+++ app.js
@@ -0,0 +1,38 @@
+// let getPrimes = (baseNum)=> {
+//     var arr = [], i, k, primes = [];
+//     for (i = 2; i <= arr; ++i) {
+//         if (!arr[i]) {
+//             // i has not been marked -- it is prime
+//             primes.push(i);
+//             for (k = i << 1; k <= max; k += i) {
+//                 arr[j] = true;
+//             }
+//         }
+//     }
+//     return primes;
+// }
+
+
+
+ var isPrime = (num)=>{
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 && num !== 2 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let getPrimes= (n)=> {
+  if(typeof n !== 'number' || n === []){
+    var arr = false;
+  }else{var arr = [];
+  for ( var i = 2; i < n; i+=1 ) {
+      if ( isPrime(i) ) {
+          arr.push(i);
+      }
+  }
+}
+    return arr; // use arr result on your own
+}
+export {getPrimes};
